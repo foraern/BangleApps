d445c6e120b577b4d7f8646d65be582fd74d9400
Foraern <foraern@users.noreply.github.com>
2020-03-31 09:34:49 +0100

    updated size and added seconds

--- apps/sclock/clock-simple.js
+++ apps/sclock/clock-simple.js
@@ -1,13 +1,12 @@
 /* jshint esversion: 6 */
-const timeFontSize = 6;
-const dateFontSize = 3;
-const gmtFontSize = 2;
+const timeFontSize = 5;
+const dateFontSize = 2;
+const gmtFontSize = 1;
 const font = "6x8";
 
 const xyCenter = g.getWidth() / 2;
-const yposTime = 75;
-const yposDate = 130;
-const yposYear = 175;
+const yposTime = 130;
+const yposDate = 175;
 const yposGMT = 220;
 
 // Check settings for what type our clock should be
@@ -17,15 +16,15 @@ function drawSimpleClock() {
   // get date
   var d = new Date();
   var da = d.toString().split(" ");
-
   g.reset(); // default draw styles
   // drawSting centered
   g.setFontAlign(0, 0);
 
   // draw time
-  var time = da[4].substr(0, 5).split(":");
+  var time = da[4].substr(0, 8).split(":");
   var hours = time[0],
-    minutes = time[1];
+  minutes = time[1],
+  seconds = time[2];
   var meridian = "";
   if (is12Hour) {
     hours = parseInt(hours,10);
@@ -41,19 +40,16 @@ function drawSimpleClock() {
   }
 
   g.setFont(font, timeFontSize);
-  g.drawString(`${hours}:${minutes}`, xyCenter, yposTime, true);
+  g.drawString(`${hours}:${minutes}:${seconds}`, xyCenter, yposTime, true);
   g.setFont(font, gmtFontSize);
   g.drawString(meridian, xyCenter + 102, yposTime + 10, true);
 
   // draw Day, name of month, Date
-  var date = [da[0], da[1], da[2]].join(" ");
+  var date = [da[0], da[1], da[2], d.getFullYear()].join(" ");
   g.setFont(font, dateFontSize);
 
   g.drawString(date, xyCenter, yposDate, true);
 
-  // draw year
-  g.setFont(font, dateFontSize);
-  g.drawString(d.getFullYear(), xyCenter, yposYear, true);
 
   // draw gmt
   var gmt = da[5];
@@ -72,7 +68,7 @@ Bangle.loadWidgets();
 Bangle.drawWidgets();
 
 // refesh every 15 sec
-setInterval(drawSimpleClock, 15E3);
+setInterval(drawSimpleClock, 1000);
 
 // draw now
 drawSimpleClock();
